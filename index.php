a02c48a67b5beea9e4e60767e809fc70bdc6627b
Giovanni Cucè <giovanni.cuce@live.it>
2017-07-07 10:01:28 +0200

    prima commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Esercizio 4</title>
+</head>
+<body>
+  
+<script src="lib/jquery-3.2.1.min.js"></script>
+<script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
